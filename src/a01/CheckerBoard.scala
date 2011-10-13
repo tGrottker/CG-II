b208b40cc1e92fefefa3443d61594680d37e74a8
Dennis Koenig <Dennis@.(none)>
2011-10-13 17:18:52 +0200

    CheckerBoard created

---
+++ src/a01/CheckerBoard.scala
@@ -0,0 +1,13 @@
+package a01
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Dennis
+ * Date: 13.10.11
+ * Time: 17:18
+ * To change this template use File | Settings | File Templates.
+ */
+
+object CheckerBoard {
+
+}
