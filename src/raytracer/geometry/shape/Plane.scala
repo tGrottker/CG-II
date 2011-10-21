57e0df015ed8b3d6c6e196b1e437dcc0f8522d74
Dennis Koenig <Dennis@.(none)>
2011-10-21 15:05:54 +0200

    Intersect method finished

--- src/raytracer/geometry/shape/Plane.scala
+++ src/raytracer/geometry/shape/Plane.scala
@@ -20,9 +20,8 @@ case class Plane(origin: Vector, normal: Vector, color: Color) extends ColoredSh
    * @inheritDoc
    */
   override def hit(ray: Ray): Boolean = {
-    // DENNIS!!!
-    // Vektor Normale x  Richtungs Vektor aus dem Ray (Skalarprodukt) != 0 => true    dot Methode
-
+    // Vektor Normale x  Richtungs Vektor aus dem Ray (Skalarprodukt) != 0 => true
+   // Prüft ob der Vektor normal und der Richtungs-Vektor des Rays sind treffen
     val s = normal.dot(ray.direction)
     if (s!=0) return true
     false
@@ -31,18 +30,17 @@ case class Plane(origin: Vector, normal: Vector, color: Color) extends ColoredSh
   /**
    * @inheritDoc
    */
+  //Methode gibt den Vektor zurück, wo sich bie beiden Vektoren schneiden
   override def intersect(ray: Ray): Vector = {
     if (!hit(ray)) throw new NoHitException("The ray: " + ray + " does not hit this shape: " + this)
-    // DENNIS!!!
-    //Seite 19
+    //Formel 1
     val z1=normal.dot(ray.origin)
     val z2=normal.dot(origin)
     val m=(z1-z2)/(normal.dot(ray.direction))
 
     //Formel 2
-    //var temp= m*ray.direction
-    //var p=ray.origin.add(temp)
-    new Vector(0,0,0)   // TODO override standard
+    val p=ray.origin.add(ray.direction.mult(m))
+    p
   }
 
   /**
