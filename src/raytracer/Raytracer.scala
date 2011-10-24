ea4e3b5c93e3b406027b0ebd9567f4dea726c12b
Dennis Koenig <Dennis@.(none)>
2011-10-24 16:52:11 +0200

    Testing the Plane class

--- src/raytracer/Raytracer.scala
+++ src/raytracer/Raytracer.scala
@@ -1,14 +1,14 @@
 package raytracer
 
 import cg2.warmup.Painter
-import geometry.shape.{ColoredShape, Sphere}
+import geometry.shape.{ColoredShape, Sphere, Plane}
 import scene.{Scene, Camera}
-import cg2.vecmath.{Vector, Color}
+import cg2.vecmath.{Color, Vector}
 
 /**
  * Main class of the raytracer.
  *
- * @author Thomas Grottker
+ * @author Thomas Grottker, Dennis Koenig
  * @date 17.10.11
  * @time 17:50
  */
@@ -16,9 +16,11 @@ class Raytracer extends Painter{
 
   val cam = new Camera(angle = 30, aspectRatio = 1)
   val sphere  = new Sphere(new Vector( 0, 0, -15), 1, new Color(1,0,0))
-  val sphere2 = new Sphere(new Vector(-1, 0, -18), 1, new Color(0,1,0))
-  val sphere3 = new Sphere(new Vector( 1, 0, -20), 1, new Color(0,0,1))
-  val scene = new Scene(List(sphere, sphere2, sphere3))
+  //val sphere2 = new Sphere(new Vector(-1, 0, -18), 1, new Color(0,1,0))
+  //val sphere3 = new Sphere(new Vector( 1, 0, -20), 1, new Color(0,0,1))
+  val plane = new Plane(new Vector( 1, 0, -10), new Vector( 1, 0, -20),new Color(1,0.5F,0))
+
+  val scene = new Scene(List(sphere, plane))
 
   override def pixelColorAt(x: Int, y: Int, nx: Int, ny: Int): Color = {
 
