188958f544c6bac72060b893ba00e3fe62fe8a98
Thomas Grottker <Thomas@.(none)>
2011-11-24 10:24:09 +0100

    AreaLightFactory added

---
+++ src/raytracer/scene/lighting/AreaLightFactory.scala
@@ -0,0 +1,20 @@
+package raytracer.scene.lighting
+
+import cg2.vecmath.Vector
+
+/**
+ *
+ *
+ * @author Thomas Grottker
+ * @date 24.11.11
+ * @time 10:15
+ */
+object AreaLightFactory {
+
+  def createPointLights(xCount: Int, yCount: Int, zCount, distanceBetweenLightSources: Float, originMin: Vector): List[PointLight] = {
+    var lights: List[PointLight] = Nil
+
+    lights
+  }
+
+}
