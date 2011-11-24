34a5e1ef3c0b495531dbeea8bc505cfd8f3dfb43
Dennis Koenig <Dennis@.(none)>
2011-11-24 11:19:00 +0100

    Adding the AreaLightFactory class

--- src/raytracer/scene/lighting/AreaLightFactory.scala
+++ src/raytracer/scene/lighting/AreaLightFactory.scala
@@ -1,20 +1,37 @@
 package raytracer.scene.lighting
 
-import cg2.vecmath.Vector
+import cg2.vecmath.{Color, Vector}
 
 /**
+ * Factory of a AreaLight
  *
- *
- * @author Thomas Grottker
+ * @author Thomas Grottker, Dennis Koenig
  * @date 24.11.11
  * @time 10:15
+ *
+ * @param xCount The max lenght in x-direction
+ * @param yCount The max lenght in y-direction
+ * @param zCount The max lenght in z-direction
+ * @param distanceBetweenLightSources The distance between two lights
+ * @param originMin Center of the AreaLight
+ * @param lightColor The Color of the AreaLight
  */
 object AreaLightFactory {
 
-  def createPointLights(xCount: Int, yCount: Int, zCount, distanceBetweenLightSources: Float, originMin: Vector): List[PointLight] = {
-    var lights: List[PointLight] = Nil
+  def createPointLights(xCount: Int, yCount: Int, zCount, distanceBetweenLightSources: Float, originMin: Vector, lightColor: Color): List[PointLight] = {
+    var lightList: List[PointLight] = Nil
+    val numberOfLights = xCount * yCount * zCount
+    val lightingColor = lightColor.modulate(1 / numberOfLights)
 
-    lights
+    for (xC <- 0 to xCount){
+      for (yC <- 0 to yCount){
+        for (zC <- 0 to zCount){
+          lightList = new PointLight(new Vector(originMin.x + xC * distanceBetweenLightSources, originMin.y + yC * distanceBetweenLightSources, originMin.z + yC * distanceBetweenLightSources), lightingColor) :: lightList
+        }
+      }
+    }
+
+    lightList
   }
 
 }
