dd1e1cb3e3bd8c72dd7eeb3feebc20168bb3d2c0
Dennis Koenig <Dennis@.(none)>
2012-01-09 12:44:41 +0100

    added the Kugel

--- src/web-gl/A04-WebGL/shapes.js
+++ src/web-gl/A04-WebGL/shapes.js
@@ -21,9 +21,9 @@
 */ 
 
 
-VertexBasedShape = function(gl, primitiveType, numVertices) {
+VertexBasedShape = function(gl, primitiveType, numVertices, transform) {
 
-    // arrays in which to store vertex buffers and the respective 
+    // arrays in which to store vertex buffers and the respective
     this.vertexBuffers = new Array();
     
     // remember what goemtric primitive to use for drawing
@@ -50,15 +50,22 @@ VertexBasedShape = function(gl, primitiveType, numVertices) {
        Method: draw using a vertex buffer object
     */
     this.draw = function(program) {
-    
-        // go through all types of vertex attributes 
+
+        var ntLocation = gl.getUniformLocation(program.glProgram,
+                                                "nodeTransform");
+        if(ntLocation == null) {
+            window.console.log("Warning: uniform nodeTransform not used in shader.");
+        } else {
+            gl.uniformMatrix4fv(ntLocation, false, transform);
+        }
+        // go through all types of vertex attributes
         // and enable them before drawing
         for(attribute in this.vertexBuffers) {
             //window.console.log("activating attribute: " + attribute);
             this.vertexBuffers[attribute].makeActive(program);
         }
-        
-        // perform the actual drawing of the primitive 
+
+        // perform the actual drawing of the primitive
         // using the vertex buffer object
         //window.console.log("drawing shape with " + 
         //                    this.numVertices + " vertices.");
@@ -78,10 +85,10 @@ VertexBasedShape = function(gl, primitiveType, numVertices) {
    
 */ 
 
-Triangle = function(gl) {
+Triangle = function(gl, transform) {
 
     // instantiate the shape as a member variable
-    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, 3);
+    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, 3, transform);
 
     var vposition = new Float32Array( [ 0,1,0,  -1,-1,0, 1,-1,0 ]);
     var vcolor    = new Float32Array( [ 1,0,0,  0,1,0,   0,0,1 ]);
@@ -104,13 +111,13 @@ Triangle = function(gl) {
    
 */ 
 
-TriangleFan = function(gl) {
+TriangleFan = function(gl, transform) {
 
     // instantiate the shape as a member variable
-    this.shape = new VertexBasedShape(gl, gl.TRIANGLE_FAN, 9);
+    this.shape = new VertexBasedShape(gl, gl.TRIANGLE_FAN, 9, transform);
 
-    var vposition = new Float32Array( [ 0,0,1,        0,1,0,       -0.7,0.7,0, 
-                                        -1,0,0,      -0.7,-0.7,0,  0,-1,0, 
+    var vposition = new Float32Array( [ 0,0,1,        0,1,0,       -0.7,0.7,0,
+                                        -1,0,0,      -0.7,-0.7,0,  0,-1,0,
                                         0.7,-0.7,0,  1.0,0,0,      0.7,0.7,0]);
     var vcolor    = new Float32Array( [ 1,1,1,  1,0,0,  0,1,0,      
                                         0,0,1,  1,0,0,  0,1,0,  
@@ -121,9 +128,10 @@ TriangleFan = function(gl) {
 }        
     
 
-Cube = function(gl) {
+Cube = function(gl, l, transform) {
 
-    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, 36);
+    var l2 = l / 2;
+    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, 36, transform);
 
     var vposition   = new Float32Array([    -1,-1,-1,    1,-1,-1,    1, 1,-1,       // abc
                                             -1,-1,-1,    1, 1,-1,   -1, 1,-1,       // acd
@@ -137,6 +145,10 @@ Cube = function(gl) {
                                              1,-1,-1,   -1,-1, 1,    1,-1, 1,       // bef
                                              1, 1,-1,   -1, 1,-1,    1, 1, 1,       // cdg
                                             -1, 1,-1,    1, 1, 1,   -1, 1, 1]);     // dgh
+
+    for (var i=0; i<108; i++){
+        vposition[i]=vposition[i] * l2;
+    }
 
     var vcolor       = new Float32Array([    1, 0, 0,    1, 0, 0,    1, 0, 0,
                                              1, 0, 0,    1, 0, 0,    1, 0, 0,
@@ -156,6 +168,91 @@ Cube = function(gl) {
 
 }
 
-Sphere = function(gl, radius, n, m, color1, color2) {
+Sphere = function (gl, radius, n, m, color1, color2, transform){
+
+    var PI = Math.PI;
+    this.radius = radius;
+
+    this.x = function (u, v) {
+        if(u < 0 || PI < u) return 0;
+        if(v < 0 || 2*PI < v) return 0;
+        return this.radius * Math.sin(u) * Math.cos(v);
+    }
+
+    this.y = function (u, v) {
+        if(u < 0 || PI < u) return 0;
+        if(v < 0 || 2*PI < v) return 0;
+        return this.radius * Math.sin(u) * Math.sin(v);
+    }
+
+    this.z  = function (u) {
+        if(u < 0 || PI < u) return 0;
+        return this.radius * Math.cos(u);
+    }
+
+    this.shape = new VertexBasedShape(gl, gl.TRIANGLES, n*m*6, transform);
+
+    var vposition = new Float32Array(n*m*6*3);
+    var vcolor = new Float32Array(n*m*6*3);
+    var index = 0;
+    var c = color1
+    for (var i = 1; i<=n; i++){
+        for (var j = 1; j<=m; j++){
+
+            c = ((i + j) % 2 == 0) ? color1 : color2
+
+            var ui1 = PI / n * i;
+            var ui0 = PI / n * (i-1);
+            var vj1 = 2 * PI / m * j;
+            var vj0 = 2 * PI / m * (j-1);
+
+            vcolor[index] = c[0];
+            vposition[index++] = this.x(ui0, vj0);
+            vcolor[index] = c[1];
+            vposition[index++] = this.y(ui0, vj0);
+            vcolor[index] = c[2];
+            vposition[index++] = this.z(ui0);
+
+            vcolor[index] = c[0];
+            vposition[index++] = this.x(ui1, vj0);
+            vcolor[index] = c[1];
+            vposition[index++] = this.y(ui1, vj0);
+            vcolor[index] = c[2];
+            vposition[index++] = this.z(ui1);
+
+            vcolor[index] = c[0];
+            vposition[index++] = this.x(ui1, vj1);
+            vcolor[index] = c[1];
+            vposition[index++] = this.y(ui1, vj1);
+            vcolor[index] = c[2];
+            vposition[index++] = this.z(ui1);
+
+
+            vcolor[index] = c[0];
+            vposition[index++] = this.x(ui0, vj0);
+            vcolor[index] = c[1];
+            vposition[index++] = this.y(ui0, vj0);
+            vcolor[index] = c[2];
+            vposition[index++] = this.z(ui0);
+
+            vcolor[index] = c[0];
+            vposition[index++] = this.x(ui0, vj1);
+            vcolor[index] = c[1];
+            vposition[index++] = this.y(ui0, vj1);
+            vcolor[index] = c[2];
+            vposition[index++] = this.z(ui0);
+
+            vcolor[index] = c[0];
+            vposition[index++] = this.x(ui1, vj1);
+            vcolor[index] = c[1];
+            vposition[index++] = this.y(ui1, vj1);
+            vcolor[index] = c[2];
+            vposition[index++] = this.z(ui1);
+
+        }
+
+        this.shape.addVertexAttribute(gl, "vertexPosition", gl.FLOAT, 3, vposition);
+        this.shape.addVertexAttribute(gl, "vertexColor",    gl.FLOAT, 3, vcolor);
+    }
 
 }
